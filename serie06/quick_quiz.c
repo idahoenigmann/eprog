3890f2fc0894745c58adfb192c023161d9e0b528
ida <idahoney@gmx.at>
2020-11-26 18:07:54 +0100

    add serie06/quick_quiz.c

--- serie06/quick_quiz.c
+++ serie06/quick_quiz.c
@@ -2,12 +2,24 @@
 // Created by ida on 20.11.20.
 //
 
-int foo() {
-    return 1;
+#include <stdio.h>
+
+void tripleSwap(double* x1, double* x2, double* x3) {
+    double tmp = *x1;
+
+    *x2 = *x3;
+    *x3 = *x1;
+    *x1 = *x2;
 }
 
 int main() {
-    foo();
+    double x = 3;
+    double y = 2;
+    double z = 1;
+
+    tripleSwap(&x, &y, &z);
+
+    printf("%f %f %f\n", x, y, z);
 
     return 0;
 }
